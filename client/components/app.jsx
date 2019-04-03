e2e49881be134c1c662bec2073ad8cdf5a34e4ef
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-04-03 10:50:37 -0500

    creating server

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -26,7 +26,7 @@ class App extends React.Component {
     };
 
     componentDidMount() {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
         .then(results => {
             const tagArr = [];
             const tagObj = {};
