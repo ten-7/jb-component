a4f593d590881867d23469ca865b026108c397d8
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-04-09 18:06:13 -0500

    fixed event listener

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -43,7 +43,7 @@ class App extends React.Component {
         })
         window.addEventListener('productId', (e) => {
             this.setState({
-                currentId: e.default
+                currentId: e.detail
             })
         })
     };
