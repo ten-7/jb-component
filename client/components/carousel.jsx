4556fffc534dfb52763ba5b1d03023ab5c053904
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-04-11 18:18:23 -0500

    aligned carousel with other services

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './styles.css';
+import Typography from '@material-ui/core/Typography'
+
 export default function Carousel(props) {
     return (
         <div className = 'carousel'>
             <div className = 'carousel_Item' onClick = {(e) => props.handleProductClick(e, props.axe.productId, props.axe.tag)}>
                 <img className= 'carousel_Pics'src={props.axe.images[0]}></img>
                     
-                <div>
-                    {props.axe.name}
+                <div className = 'carousel_text'>
+                    <Typography variant='body2'>
+                        {props.axe.name}
+
+                    </Typography>
                 </div>
             </div>
         </div>
